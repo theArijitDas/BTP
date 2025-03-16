6949d46b73e995220a03da66311a709d2ddea249
maheshg11 <maheshghumare1234@gmail.com>
2025-03-16 10:06:57 +0530

    Update: Model Utils and Data Generation

--- Code/DataCode/data_load.py
+++ Code/DataCode/data_load.py
@@ -3,5 +3,52 @@ data generator
 + datagenerator(seed, maximum number of files)
 + generating at every time instant a random data(): what probability distribution to use - simple
 + extreme random
-
 '''
+import random
+import os
+import numpy as np
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
+
+class RandomDataGenerator:
+    def __init__(self, N: int, distribution: str="uniform", seed: int=42,oneHotEncoded: bool=False):
+        seed_everything(seed)
+        self.distribution=distribution.lower()
+        self.N=N
+        self.oneHotEncoded=oneHotEncoded
+    def getOneHotEncoded(self, a:int):
+        '''
+        generates one hot encoded vector(numpy array)
+        '''
+        zeros_array = np.zeros(self.N, dtype=np.int32)
+        zeros_array[a]=1
+        return zeros_array
+    
+    def generateFromDistribution(self):
+        '''
+            generates a pair of number from the given distribution
+        '''
+        if(self.distribution=="uniform"):return np.random.randint(0, self.N, size=2)
+        if(self.distribution=="normal"):
+            arr=np.random.normal(loc=self.N/2, scale=self.N/6, size=2).astype(np.int32)
+            i=1
+            while i>=0:
+                if(arr[i]>=self.N):arr[i]=self.N-1
+                elif arr[i]<0: arr[i]=0
+                i-=1
+            return arr
+
+
+    def generate(self):
+        '''
+        returns list of one hot encoded vectors if one hot encoded is true
+        returns list of size 2 of integers 
+        '''
+        arr=self.generateFromDistribution()
+        if(self.oneHotEncoded):
+            return [self.getOneHotEncoded(arr[0]),self.getOneHotEncoded(arr[1])]
+        return arr
--- Code/Models/model_utils.py
+++ Code/Models/model_utils.py
@@ -3,3 +3,49 @@ static optimum
 regret calculation 
 models - FTPL
 '''
+import numpy as np
+class ModelUtils():
+    
+    def __init__(self,N: int,oneHotEncoded: bool=False):
+        self.oneHotEncoded=oneHotEncoded
+        self.N=N
+        self.freqMatrix=np.zeros((N,N), dtype=np.int32) # reward matrix in presentation have discussion over the name before u change it
+        self.maxValue=0
+        self.maxPosition=[0,0]
+        self.n=0 #number of time the reward matrix was updated or number of instances that have passed
+        self.reward=0 # cumulative reward till this point
+    
+    def getIntegerFromOneHotEncoded(self,a: list[np.ndarray]):
+        arr=[-1,-1]
+        
+        for d in range(2): # d stands for Dimension
+            for i in range(len(a[d])):
+                if(a[d][i]):
+                    arr[d]=i
+                    break
+        return arr             
+
+    def updatefreqMatrix(self,a):
+        if(self.oneHotEncoded):a=self.getIntegerFromOneHotEncoded(a) # thiss all marked as "thiss" are expected to be list of two one hot encoded vectors
+        self.freqMatrix[a[0],a[1]]+=1
+        if(a[0]!=a[1]):
+            self.freqMatrix[a[1],a[0]]+=1
+        if(self.maxValue<=self.freqMatrix[a[0],a[1]]):
+            self.maxValue=self.freqMatrix[a[0],a[1]]
+            self.maxPosition=[a[0],a[1]]
+        self.n+=1
+
+    def getStaticOptimumReward(self): # refers to the reward corresponding to best possible choice 
+        '''
+        May be implemented incorrectly : first discuss then change
+        '''
+        return self.maxValue
+    
+    def updateReward(self,predicted,requested):
+        if(self.oneHotEncoded):
+            requested=self.getIntegerFromOneHotEncoded(requested) # thiss
+            predicted=self.getIntegerFromOneHotEncoded(predicted) # thiss
+        predicted.sort()
+        requested.sort()
+        self.reward+= (predicted[0]==requested[0] and predicted[1]==requested[1])
+
